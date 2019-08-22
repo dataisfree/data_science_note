928e323f7bd3206c968e492973e1509ec7e45c98
chenzhiwei <chenzhiwei@zealfi.com>
2019-08-22 15:25:12 +0800

    add simple plot case

--- numpy_note/matplotlib_base_numpy.py
+++ numpy_note/matplotlib_base_numpy.py
@@ -1,4 +1,24 @@
 # coding: utf-8
 """
+基于numpy的数据可视化
+"""
+
+import os
+import numpy as np
+print(np.__version__)
+import matplotlib.pyplot as plt
 
 """
+matplotlib.pyplot提供了简单的绘图功能，
+调用的函数都会改变当前的绘图，通过save/show完成保存或显示功能
+"""
+
+# 绘图入门
+func = np.poly1d(np.array([1, 2, 3, 4]).astype('float64'))
+x = np.linspace(-10, 10, 30)
+y = func(x)
+
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y=f(x)')
+plt.show()
